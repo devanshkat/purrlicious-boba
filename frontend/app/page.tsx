c917ceaeba02be4d87d5fae54a2926fe1402c7dd
AbheekGandhi <abheekgandhi20@gmail.com>
2024-08-12 12:13:55 -0700

    Added logo
    Logo component has been added to page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Logo from "../public/Images/logo.png";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
+        <img src={Logo.src} alt="Logo" />
     </main>
   );
 }
+
