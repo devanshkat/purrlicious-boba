b6bbb1008d6ebe13a706bfe25ba2d0d7470ea3b3
akarshlegala <akarshlegala@gmail.com>
2024-09-08 16:54:30 -0700

    Our Boba Page
    frontend:
    - implemented drink menu
    - added transitions
    
    *still need to make everything look nicer
    
    Bollu + ARL teamwork makes the dreamwork

---
+++ frontend/app/boba/components/Nav.tsx
@@ -0,0 +1,14 @@
+
+
+const Nav = () => {
+    return (
+        <div className="m-[5em] h-full flex flex-row justify-between item-center">
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+
+export default Nav;
