ace70e50653be2d2d08b0c6f85c652e128a6b7b1
RaspingHare3640 <adithya.bollu@gmail.com>
2024-09-09 10:14:25 -0700

    Updated EmployeeList
    - Changed the font to be bigger

--- frontend/app/about/components/EmployeeList.tsx
+++ frontend/app/about/components/EmployeeList.tsx
@@ -8,7 +8,7 @@ interface EmployeeListProps {
 const EmployeeList : React.FC<EmployeeListProps> = ({emList}) => {
   return (
     <div className="p-5">
-      <div className="text-[2rem] font-bold text-black">Our Story</div>
+      <div className="text-[4rem] font-bold text-black">Our Story</div>
       <div className="text-black">At Purrlicious Boba , we’re more than just a place to grab a drink—we dedicated to bring everyone  joy through 
         delicious  boba teas and the love of animals. Here, you can sip on our homemade crafted boba tea while enjoying 
         the company of our friendly  cats. We believe that spending time with animals can uplift spirits and improve mental 
