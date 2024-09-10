ef28597f53fad7bec1e667d142cb8476bebcf407
arrrnav <arnav.chatty@gmail.com>
2024-09-09 17:15:43 -0700

    Made the logo link back to home page

--- frontend/app/components/Nav.tsx
+++ frontend/app/components/Nav.tsx
@@ -5,7 +5,9 @@ import Link from "next/link"
 const Nav = () => {
   return (
     <div className="flex flex-row justify-between items-center bg-orange-300 p-4 top-0 w-full h-1/8">
+      <Link href={"/"}>
       <img className="rounded-[3.5rem]" src={Logo.src} alt="Logo" width={"110rem"} height={"110rem"}/>
+      </Link>
       <div className="flex flex-row justify-between text-center items-center w-[50%] text-[90%] text-black">
         <div className=""><Link href={"/"}>Home</Link></div>
         <div className=""><Link href={"/about"}>About Us</Link></div>
