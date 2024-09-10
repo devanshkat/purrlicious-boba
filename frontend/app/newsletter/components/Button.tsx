ccc2e9a087fe2afc0044f4379f7a6670abd9398e
Devansh Katiyar <devanshkat@gmail.com>
2024-09-09 18:11:15 -0700

    Implemented Newsletter Page
    - Added image
    - Added input box
    - Added submission button

---
+++ frontend/app/newsletter/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from "../../public/Images/logo.png";
+import Link from "next/link"
+
+interface ButtonProps {
+    buttontxt: string;
+    page: string;
+}
+
+const Button : React.FC<ButtonProps> = ({buttontxt, page}) => {
+  return (
+    <Link href={page}>
+    <div className="flex justify-center items-center text-[2vmin] text-center w-[10vw] h-[7vh] bg-[#FFB7CE] p-3 rounded-[2em] text-black font-bold m-5 transition-rotate transition-colors duration-500 ease-in-out hover:bg-orange-400 hover:text-white cursor-pointer">{buttontxt}</div>
+    </Link>
+  );
+};
+
+export default Button;
--- frontend/app/newsletter/page.tsx
+++ frontend/app/newsletter/page.tsx
@@ -1,5 +1,8 @@
 import Nav from "../components/Nav"
 import Footer from "../components/Footer"
+import Table from "../../public/Images/purrlicious_tabling.png"
+import Button from "./components/Button"
+import TextImage from ".././components/TextImage"
 
 
 export default function NewsletterPage() {
@@ -9,8 +12,13 @@ export default function NewsletterPage() {
         <Nav />
       </div>
 
-      <div className="flex flex-col items-center p-4">
+      <div className="flex flex-col items-center p-4 space-y-6">
         <h1 className="font-inknut text-center text-black text-[3.8em] font-bold">Join our Newsletter</h1>
+        <img src={Table.src} alt="" width={"650px"}></img>
+      </div>
+      <div className="flex flex-row items-center justify-center space-x-1">
+        <input className="w-[25vw] h-[7vh] rounded-[5em] outline-none text-black pl-6" type={"text"} id="email" placeholder="Enter your email"></input>
+        <Button buttontxt="Get Notified!" page="/newsletter"/>
       </div>
       
       <div className="mt-auto h-full w-full">
