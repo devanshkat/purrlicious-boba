5f6d93256aef9324752523b3d7ebdc50aac48540
RaspingHare3640 <adithya.bollu@gmail.com>
2024-09-10 09:16:53 -0700

    Updated Nav Buttons
    - Changed border to orange
    - Added a transition so that the border expands from the center
    
    * Border animation can be change from left or right as well

--- frontend/app/components/NavButton.tsx
+++ frontend/app/components/NavButton.tsx
@@ -10,9 +10,12 @@ interface NavButtonProps {
 const NavButton : React.FC<NavButtonProps> = ({buttontxt, page}) => {
   return (
     <Link href={page}>
-    <div className="text-[2vmin] text-center font-bold text-black border-b-2 border-transparent transition-all duration-500 ease-in-out hover:border-black focus:border-black"
+    <div className="text-[2vmin] p-1 text-center font-bold text-black border-b-2 border-transparent
+    transition-all duration-500 ease-in-out relative after:content-[''] after:absolute after:left-0 
+    after:bottom-0 after:h-[3px] after:w-full after:bg-orange-600 after:scale-x-0 after:origin-center 
+    after:transition-transform after:duration-500 hover:after:scale-x-100"
         style = {{
-            transform: 'scaleY(1.25)'
+            transform: "scaleY(1.25)"
         }}
     >{buttontxt}</div>
     </Link>
