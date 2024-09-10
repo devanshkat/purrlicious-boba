646ac5a23595e559786fc77d6f3f5b035430b8ac
AbheekGandhi <abheekgandhi20@gmail.com>
2024-09-10 14:57:30 -0700

    Added Contact Us button

--- frontend/app/contact/components/ContactForm.tsx
+++ frontend/app/contact/components/ContactForm.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Button from "../../components/Button";
 
 const ContactForm = () => {
+
+    
     return ( 
         <div className="rounded-lg flex flex-col justify-between bg-white p-4 gap-[3rem]">
             <div className="flex flex-col gap-[1rem] w-[30vw]">
@@ -20,6 +22,9 @@ const ContactForm = () => {
                 <p className="flex font-inknut text-left text-black text-[1em] font-regular">Message:</p>
                 {/* <input className="flex outline-none p-2 text-black border-b-2 transition-all duration-500 ease-in-out hover:border-orange-400 focus:border-orange-400" type="text" name="name" placeholder="Value"></input> */}
                 <textarea className='rounded-[0.5em] min-h-[25vh] max-h-[60vh] flex outline-none p-2 text-black border-2 transition-all duration-500 ease-in-out hover:border-orange-400 focus:border-orange-400' name="" id=""></textarea>
+            </div>
+            <div className="flex justify-center">
+                <Button buttontxt="Contact Us" page="#" />
             </div>
         </div>
     );
