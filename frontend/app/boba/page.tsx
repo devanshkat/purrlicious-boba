bbab2c6e1e4b5159d97f3a0acc837d6e4f9e03b4
Devansh Katiyar <devanshkat@gmail.com>
2024-09-11 16:56:18 -0700

    Added Thai Green and Mango Green
    - Added Thai Green and Mango Green to the Our Boba Page

--- frontend/app/boba/page.tsx
+++ frontend/app/boba/page.tsx
@@ -3,10 +3,11 @@ import Nav from "../components/Nav"
 import Footer from "../components/Footer"
 import BobaCard from './components/BobaCard';
 import thai from "../../public/Images/thai.jpg"
+import green from "../../public/Images/green.jpg"
 import taro from "../../public/Images/taro.jpg"
+import mango from "../../public/Images/mango.jpg"
 import strawberry from "../../public/Images/strawberry.jpg"
 import ube from "../../public/Images/ube.jpg"
-import boba1 from "../../public/Images/BobaTea.jpg"
 
 export default function OurBobaPage() {
     return (
@@ -21,9 +22,9 @@ export default function OurBobaPage() {
 
       <div className="flex flex-row flex-wrap justify-center items-center">
         <BobaCard name="Thai Milk Tea" price="5.00" imageloc={thai.src} description= "Ingredients: Water, Sugar, Thai Tea Mix (Tea, Food Coloring [Yellow 6]), Sweetened Condensed Milk (Milk, Sugar), Evaporated Milk (Milk, Dipotassium Phosphate, Carrageenan, Vitamin D3), Half-and- Half (Milk, Cream), Natural and Artificial Flavorings" fontSize={1.7}></BobaCard>
-        <BobaCard name="Thai Green Tea" price="5.00" imageloc={boba1.src} description= "Ingredients: Water, Sugar, Thai Green Tea Mix (Green Tea, Food Coloring [Blue 1, Yellow 5]), Sweetened Condensed Milk (Milk, Sugar), Evaporated Milk (Milk, Dipotassium Phosphate, Carrageenan, Vitamin D3), Half-and-Half (Milk, Cream), Natural and Artificial Flavorings" fontSize={1.7}></BobaCard>
+        <BobaCard name="Thai Green Tea" price="5.00" imageloc={green.src} description= "Ingredients: Water, Sugar, Thai Green Tea Mix (Green Tea, Food Coloring [Blue 1, Yellow 5]), Sweetened Condensed Milk (Milk, Sugar), Evaporated Milk (Milk, Dipotassium Phosphate, Carrageenan, Vitamin D3), Half-and-Half (Milk, Cream), Natural and Artificial Flavorings" fontSize={1.7}></BobaCard>
         <BobaCard name="Taro Milk Tea" price="5.00" imageloc={taro.src} description= "Sweetended taro non-dairy creamer powder (sugar, glucose syrup, Hydrogenated palm kernel oil, Whey (milk), Taro powder, artificial flavors, maltodextrin, sodium caseinate, silicon dioxide, Dipotassium phosphate, tripotassium phosphate, mono & diglyceride, sodium tripolyphosphate, diacetyl tartaric acid esters of mono & diglycerides (Datem), Beta carotene (color), suctalose, blue 1, red 3, red 40." fontSize={1.7}></BobaCard>
-        <BobaCard name="Mango Green Tea" price="5.00" imageloc={boba1.src} description= "Mango" fontSize={1.7}></BobaCard>
+        <BobaCard name="Mango Green Tea" price="5.00" imageloc={mango.src} description= "Mango" fontSize={1.7}></BobaCard>
         <BobaCard name="Strawberry Green Tea" price="5.00" imageloc={strawberry.src} description= "Strawberry" fontSize={1.6}></BobaCard>
         <BobaCard name="Ube Milk Tea" price="5.00" imageloc={ube.src} description= "Ube" fontSize={1.7}></BobaCard>
       </div>
