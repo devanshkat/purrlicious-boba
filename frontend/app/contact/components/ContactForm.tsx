62b531dc9ba64855830ac2e4744b7aa5f2883faa
Daniel Guo <dguo314other@gmail.com>
2024-09-16 15:42:06 -0700

    Adapt Contact Us page for mobile
    - Changed margins and flex direction using sm: tag
    
    * Logo image may change

--- frontend/app/contact/components/ContactForm.tsx
+++ frontend/app/contact/components/ContactForm.tsx
@@ -6,19 +6,19 @@ const ContactForm = () => {
     
     return ( 
         <div className="rounded-lg flex flex-col justify-between bg-white p-4 gap-[3rem]">
-            <div className="flex flex-col gap-[1rem] w-[30vw]">
+            <div className="flex flex-col gap-[1rem] w-full sm:w-[30vw]">
                 <p className="flex font-inknut text-left text-black text-[1em] font-regular">First Name:</p>
                 <input className="flex outline-none p-2 text-black border-b-2 transition-all duration-500 ease-in-out hover:border-orange-400 focus:border-orange-400" type="text" name="name" placeholder="Value"></input>
             </div>
-            <div className="flex flex-col gap-[1rem] w-[30vw]">
+            <div className="flex flex-col gap-[1rem] w-full sm:w-[30vw]">
                 <p className="flex font-inknut text-left text-black text-[1em] font-regular">Last Name:</p>
                 <input className="flex outline-none p-2 text-black border-b-2 transition-all duration-500 ease-in-out hover:border-orange-400 focus:border-orange-400" type="text" name="name" placeholder="Value"></input>
             </div>
-            <div className="flex flex-col gap-[1rem] w-[30vw]">
+            <div className="flex flex-col gap-[1rem] w-full sm:w-[30vw]">
                 <p className="flex font-inknut text-left text-black text-[1em] font-regular">Email:</p>
                 <input className="flex outline-none p-2 text-black border-b-2 transition-all duration-500 ease-in-out hover:border-orange-400 focus:border-orange-400" type="text" name="name" placeholder="Value"></input>
             </div>
-            <div className="flex flex-col gap-[1rem] w-[30vw]">
+            <div className="flex flex-col gap-[1rem] w-full sm:w-[30vw]">
                 <p className="flex font-inknut text-left text-black text-[1em] font-regular">Message:</p>
                 {/* <input className="flex outline-none p-2 text-black border-b-2 transition-all duration-500 ease-in-out hover:border-orange-400 focus:border-orange-400" type="text" name="name" placeholder="Value"></input> */}
                 <textarea className='rounded-[0.5em] min-h-[25vh] max-h-[60vh] flex outline-none p-2 text-black border-2 transition-all duration-500 ease-in-out hover:border-orange-400 focus:border-orange-400' name="" id=""></textarea>
--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -11,14 +11,16 @@ export default function ContactPage() {
         <Nav showTitle={true}/>
       </div>
 
-      <div className="flex flex-col items-center p-4 mt-40">
-        <h1 className="font-inknut text-center text-black text-[3.8em] font-bold">Contact Us</h1>
-        <h2 className="font-inknut text-center text-[#2C2C2C] text-[2em] font-regular">For Business Inquiries: Catering, Events, and More</h2>
+      <div className="flex flex-col items-center p-4 mt-40 sm:mt-40">
+        <h1 className="font-inknut text-center text-black text-[2em] sm:text-[3.8em] font-bold">Contact Us</h1>
+        <h2 className="font-inknut text-center text-[#2C2C2C] text-[1em] sm:text-[2em] font-regular">For Business Inquiries: Catering, Events, and More</h2>
       </div>
 
-      <div className="flex flex-row items-center m-[4rem] justify-center gap-[15rem]">
-        <img src={Logo.src} alt="Logo" width={"440rem"} height={"440rem"}/>
-        <ContactForm/>
+      <div className="flex flex-col sm:flex-row items-center m-[2rem] sm:m-[4rem] justify-center gap-[2rem] sm:gap-[15rem]">
+        <img src={Logo.src} alt="Logo" className="w-5/6 sm:w-1/3"/>
+        <div className="w-full"> 
+          <ContactForm/>
+        </div>
       </div>
       
       <div className="mt-auto h-full w-full">
