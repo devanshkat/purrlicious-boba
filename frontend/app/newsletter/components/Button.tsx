b9b5bea43d50dce3fdff649ab09c7784ebf49fb9
akarshlegala <akarshlegala@gmail.com>
2024-09-16 15:14:21 -0700

    made newsletter page mobile friendly

--- frontend/app/newsletter/components/Button.tsx
+++ frontend/app/newsletter/components/Button.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React from "react";
 import Logo from "../../public/Images/logo.png";
-import Link from "next/link"
+import Link from "next/link";
 
 interface ButtonProps {
-    buttontxt: string;
-    page: string;
-    onClick: any;
+  buttontxt: string;
+  page: string;
+  onClick: any;
 }
 
-const Button : React.FC<ButtonProps> = ({buttontxt, page, onClick}) => {
+const Button: React.FC<ButtonProps> = ({ buttontxt, page, onClick }) => {
   return (
     <Link href={page}>
-    <div onClick={onClick} className="flex justify-center items-center text-[2vmin] text-center w-[10vw] h-[7vh] bg-[#FFB7CE] p-3 rounded-[2em] text-black font-bold m-5 transition-rotate transition-colors duration-500 ease-in-out hover:bg-orange-400 hover:text-white cursor-pointer">{buttontxt}</div>
+      <div
+        onClick={onClick}
+        className="
+          flex justify-center 
+          items-center 
+          text-[4vmin]          
+          md:text-[2vmin]      
+          text-center 
+          w-[35vw]
+          md:w-[10vw]           
+          h-[5.5vh]             
+          md:h-[7vh]           
+          bg-[#FFB7CE] 
+          p-4                  
+          md:p-3             
+          rounded-[2em] 
+          text-black font-bold 
+          m-3                 
+          md:m-5             
+          transition-rotate 
+          transition-colors 
+          duration-500 
+          ease-in-out 
+          hover:bg-orange-400 
+          hover:text-white 
+          cursor-pointer
+        "
+      >
+        {buttontxt}
+      </div>
     </Link>
   );
 };
--- frontend/app/newsletter/page.tsx
+++ frontend/app/newsletter/page.tsx
@@ -1,42 +1,64 @@
 "use client";
-import Nav from "../components/Nav"
-import Footer from "../components/Footer"
-import Table from "../../public/Images/purrlicious_tabling.png"
-import Button from "./components/Button"
-import TextImage from ".././components/TextImage"
-import {useState, useEffect} from "react";
-
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import Table from "../../public/Images/purrlicious_tabling.png";
+import Button from "./components/Button";
+import TextImage from ".././components/TextImage";
+import { useState, useEffect } from "react";
 
 export default function NewsletterPage() {
   const [email, setEmail] = useState("");
-    async function signUpEmail(e: any) {
-      let response = await fetch("/api/email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email })
-      })
-    }
+  async function signUpEmail(e: any) {
+    let response = await fetch("/api/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+  }
 
-    return (
+  return (
     <div className="flex flex-col min-w-screen min-h-screen w-full h-full bg-[#FED8B1]">
       <div className="">
-        <Nav showTitle={true}/>
+        <Nav showTitle={true} />
       </div>
 
       <div className="flex flex-col items-center p-4 space-y-6 mt-40">
-        <h1 className="font-inknut text-center text-black text-[3.8em] font-bold">Join our Newsletter</h1>
+        <h1 className="font-inknut text-center text-black text-[3.8em] font-bold">
+          Join our Newsletter
+        </h1>
         <img src={Table.src} alt="" width={"650px"}></img>
       </div>
       <div className="flex flex-row items-center justify-center space-x-1">
-        <input onChange={ (e) => setEmail(e.target.value)} className="w-[25vw] h-[7vh] rounded-[5em] outline-none text-black pl-6" type={"text"} id="email" placeholder="Enter your email"></input>
-        <Button onClick={signUpEmail} buttontxt="Get Notified!" page="/newsletter"/>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          className="
+          w-[80%]     
+          md:w-[20vw]  
+          h-[5vh]    
+          md:h-[7vh]  
+          rounded-[5em] 
+          outline-none 
+          text-black 
+          p-4    
+          ml-4
+          sm:pl-6    
+        "
+          type="text"
+          id="email"
+          placeholder="Enter your email"
+        />
+        <Button
+          onClick={signUpEmail}
+          buttontxt="Get Notified!"
+          page="/newsletter"
+        />
       </div>
-      
+
       <div className="mt-auto h-full w-full">
         <Footer />
       </div>
     </div>
-    );
+  );
 }
