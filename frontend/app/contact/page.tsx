9c10420f976eb15ce015510a80c6e2a895558a32
Daniel Guo <dguo314other@gmail.com>
2024-09-16 19:55:30 -0700

    Fix typo in Contact Us page
    - Changed width of contact form to w-auto when in large screen

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -11,14 +11,14 @@ export default function ContactPage() {
         <Nav showTitle={true}/>
       </div>
 
-      <div className="flex flex-col items-center p-4 mt-40 sm:mt-40">
+      <div className="flex flex-col items-center p-4 mt-40">
         <h1 className="font-inknut text-center text-black text-[2em] sm:text-[3.8em] font-bold">Contact Us</h1>
         <h2 className="font-inknut text-center text-[#2C2C2C] text-[1em] sm:text-[2em] font-regular">For Business Inquiries: Catering, Events, and More</h2>
       </div>
 
       <div className="flex flex-col sm:flex-row items-center m-[2rem] sm:m-[4rem] justify-center gap-[2rem] sm:gap-[15rem]">
         <img src={Logo.src} alt="Logo" className="w-5/6 sm:w-1/3"/>
-        <div className="w-full"> 
+        <div className="w-full sm:w-auto"> 
           <ContactForm/>
         </div>
       </div>
