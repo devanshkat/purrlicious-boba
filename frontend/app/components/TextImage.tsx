41f71637418054b7335a56eaf15f6029b7d8ef87
Devansh Katiyar <devanshkat@gmail.com>
2024-09-18 16:12:05 -0700

    Fixed Text-Image Width Issue
    - Fixed text-image issue where the image would take up the width of the whole page

--- frontend/app/components/TextImage.tsx
+++ frontend/app/components/TextImage.tsx
@@ -15,7 +15,7 @@ const TextImage : React.FC<TextImageProps> = ({head, content, buttontxt, image, 
 
     return (
         <div className={`flex min-w-full justify-evenly m-1 ${orientation === 0 ? "flex-row" : "flex-row-reverse"}`}>
-            <img className="w-[100%] lg:w-[100vmin] min-h-full lg:h-[75vmin] ml-1 mr-1" src={image}/>
+            <img className="w-[50%] lg:w-[100vmin] min-h-full lg:h-[75vmin] ml-1 mr-1" src={image}/>
             <div className='flex flex-col justify-center items-center w-[100%] lg:w-[50%] m-1'>
                 <h1 className={"text-[6vmin] text-center font-bold text-black lg:w-[100%]"}>{head}</h1>
                 <p className='flex text-center text-black text-[3vmin] w-[100%] lg:w-[60%]'>{content}</p>
