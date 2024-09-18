64ce45265a33c1c507d8c218cb78e145888e066a
arrrnav <arnav.chatty@gmail.com>
2024-09-18 16:58:11 -0700

    Hamburger Menu is functional - not pretty (aditha help with styling a bit)

--- frontend/app/components/Hamburger.tsx
+++ frontend/app/components/Hamburger.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react';
-import { RxCross2 } from "react-icons/rx";
+import NavButtonMobile from "./NavButtonMobile";
+// import { RxCross2 } from "react-icons/rx";
 
 interface HamburgerProps {
     onClose: () => void;
@@ -14,10 +15,18 @@ const Hamburger: React.FC<HamburgerProps> = ({onClose}) => {
     };
     
     return (
-        <div className="fixed mt-11 left-0 w-full h-full bg-orange-300 z-50">
-            <button onClick={onClose}>
+        <div className="fixed transition-all duration-500 ease-in-out mt-11 left-0 w-full h-3/5 bg-orange-300 z-50">
+            {/* <button onClick={onClose}>
                 <h2 className='text-white'>Hamburger</h2>
-            </button>
+            </button> */}
+            <div className="flex-col mt-[20%] transition-all duration-500 ease-in-out justify-between text-center items-center h-[60%] text-[90%] text-black">
+                {/* <NavButton buttontxt='Home' page="/"/> */}
+                <NavButtonMobile buttontxt='Our Boba' page="/boba"/>
+                <NavButtonMobile buttontxt='About Us' page="/about"/>
+                <NavButtonMobile buttontxt='Contact Us' page="/contact"/>
+                <NavButtonMobile buttontxt='Join our Newsletter' page="/newsletter"/>
+                {/* <div className="bg-[#FFB7CE] pt-[0.2rem] pb-[0.2rem] pl-[0.5rem] pr-[0.5rem] rounded-3xl"><Link href={"/newsletter"}>Join Our Newsletter</Link></div> */}
+            </div>
         </div>
     );
 };
---
+++ frontend/app/components/NavButtonMobile.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from "../../public/Images/logo.png";
+import Link from "next/link"
+
+interface NavButtonMobileProps {
+    buttontxt: string;
+    page: string;
+}
+
+const NavButtonMobile : React.FC<NavButtonMobileProps> = ({buttontxt, page}) => {
+  return (
+    <Link href={page}>
+    <div className="text-[5vmin] p-5 text-center font-bold text-black border-b-2 border-transparent"
+        style = {{
+            transform: "scaleY(1.25)"
+        }}
+    >{buttontxt}</div>
+    </Link>
+  );
+};
+
+export default NavButtonMobile;
