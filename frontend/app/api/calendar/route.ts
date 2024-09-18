e9770f7608701e65ac0f32313cd308148ae3c669
RaspingHare3640 <adithya.bollu@gmail.com>
2024-09-17 20:12:59 -0700

    Small updates to API

--- frontend/app/api/calendar/route.ts
+++ frontend/app/api/calendar/route.ts
@@ -7,10 +7,14 @@ export async function GET( ) {
     const key = process.env.GOOGLE_API_KEY;
     try {
         const response = await fetch(`https://www.googleapis.com/calendar/v3/calendars/cab1502e4c29c6a0108e1a52aeb75d5ff0e50f1671147ce108041889851ba3c4@group.calendar.google.com/events?key=${key}`, {
+            method: "GET",
             headers: {
-                'Cache-Control': 'no-cache'
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',  // Additional header for older HTTP/1.0 proxies
+                'Expires': '0' ,        // Expire immediately
             }
         });
+
 
         let calendarData = []
         const data = await response.json();
@@ -35,6 +39,8 @@ export async function GET( ) {
                     obj.title = item.summary;
                     obj.id = item.id;
                     obj.location = item.location.split(",").slice(0, 2).join();
+
+                    console.log(item.location)
                     
                     
                     
